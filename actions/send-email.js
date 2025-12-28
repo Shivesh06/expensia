030ef3cf0fd3c72993eeb495c6a997a912ace629
Shivesh06 <shiveshkumardey@gmail.com>
2025-12-28 16:23:21 +0530

    updated send-email

--- actions/send-email.js
+++ actions/send-email.js
@@ -6,16 +6,20 @@ export async function sendEmail({ to, subject, react }) {
   const resend = new Resend(process.env.RESEND_API_KEY || "");
 
   try {
-    const data = await resend.emails.send({
+    const { data, error } = await resend.emails.send({
       from: "Finance App <onboarding@resend.dev>",
       to,
       subject,
       react,
     });
+    if (error) {
+      console.error("Resend API Error:", error);
+      throw new Error(error.message);
+    }
 
     return { success: true, data };
   } catch (error) {
     console.error("Failed to send email:", error);
-    return { success: false, error };
+    throw error; 
   }
 }
